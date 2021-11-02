235a55008792ede99c437aac8e6e286a8681f73b
Tank <johnson90.cm@gmail.com>
2021-11-02 12:34:47 -0600

    tests, lib, index

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,13 +1,13 @@
 const Engineer = require("../lib/Engineer");
 
 test("Ability to set GitHub username using constructor function", () => {
-    const github = "malloryfaria";
-    const employee = new Engineer("Mallory", 1, "mallory.faria@gmail.com", github);
+    const github = "janedoe ";
+    const employee = new Engineer("Jane", 1, "jane.doe@gmail.com", github);
     expect(employee.github).toBe(github);
   });
 
   test("getRole() should return Engineer as a role", () => {
     const role = "Engineer";
-    const employee = new Engineer("Mallory", 1, "mallory.faria@gmail.com", "malloryfaria");
+    const employee = new Engineer("Jane", 1, "jane.doe@gmail.com", "janedoe");
     expect(employee.getRole()).toBe(role);
   });
--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -2,13 +2,13 @@
 const Intern = require("../lib/Intern");
 
 test("Ability to set school using constructor function", () => {
-    const school = "Hogwarts School of Witchcraft";
-    const employee = new Intern("Mallory", 1, "mallory.faria@gmail.com", school);
+    const school = "Harvard";
+    const employee = new Intern("Jane", 1, "jane.doe@gmail.com", school);
     expect(employee.school).toBe(school);
   });
 
   test("getRole() should return Intern as a role", () => {
     const role = "Intern";
-    const employee = new Intern("Mallory", 1, "mallory.faria@gmail.com", "malloryfaria");
+    const employee = new Intern("Jane", 1, "jane.doe@gmail.com", "jane");
     expect(employee.getRole()).toBe(role);
   });
